ed9e13cc084b01cc07ee26559874f1c205603605
Ous Bayaa <bayaaous@gmail.com>
2020-03-13 15:00:02 -0700

    added class component from line 7 till line 24 (commented out)

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,25 @@ import axios from 'axios';
 import PlayerData from './Components/PlayerData.js';
 import NavBar from './Components/NavBar';
 
+// class App extends Component {
+//   constructor() {
+//   super()
+//   this.state= {
+//     players: [],
+//     }
+//   }
+//   componentDidMount() {
+//     axios
+//     .get(''http://localhost:5000/api/players'')
+//     .then(res => {
+//       this.setState({
+//         players: res.data
+//       })
+
+//     })
+      
+//   render(){}
+
 function App() {
   const [players, setPlayers] = useState([{}])
 
